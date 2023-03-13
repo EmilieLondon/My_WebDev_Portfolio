3383809390af9aed3efee49808b85ebae081887e
Emilie Godin-Depaul <em.godin80@yahoo.com>
2023-03-13 11:02:28 +0000

    styling cards and buttons, to be continued

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,32 +1,39 @@
 import React from "react";
 import projects from "../projects.json";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
 
 function Portfolio(props) {
   return (
-    <Card style={{ width: '18rem' }}>
-
+    <Row xs={1} md={2} className="g-4">
+    {Array.from({ length: 1 }).map((_, idx) => (
+      <Col>
+      <div>
+    <Card>
       {projects.map((item, index) => {
         return (
-          <div key={item.id}>
-      <Card.Img variant="top" src={process.env.PUBLIC_URL + item.image} />
-      <Card.Body>
+          <div  className="cardContainer" key={item.id}>
+            <Card.Img variant="top" src={process.env.PUBLIC_URL + item.image} />
+            <Card.Body>
               <Card.Title>{item.title}</Card.Title>
-         <Card.Text>
-         {item.description}
-        </Card.Text>
-          </Card.Body>
-
-        <Card.Body>
-        <Card.Link href={item.link}>Website</Card.Link>
-        <Card.Link href={item.repo}>GitHub</Card.Link>
-        </Card.Body>
-        </div>
-          );
+              <Card.Text>{item.description}</Card.Text>
+            <div className="buttonContainer">
+               <Button variant="primary" size="lg" active href={item.link}>Website</Button>
+              <Button variant="secondary" size="lg" href={item.repo}>GitHub</Button>
+              </div>
+                                     </Card.Body>
+          </div>
+        );
       })}
     </Card>
-    );
-    }
+    </div>
+    </Col>
+    ))}
+    </Row>
+  );
+}
 
-   
 export default Portfolio;
